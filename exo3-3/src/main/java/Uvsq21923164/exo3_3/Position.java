f5606b0c8c4dabe913449d6b12c05cf1a9fb1156
KEBIR Ramdane <ramdane.kebir@outlook.fr>
2020-03-20 14:50:35 +0100

    creation de la classe position

---
+++ exo3-3/src/main/java/Uvsq21923164/exo3_3/Position.java
@@ -0,0 +1,29 @@
+package Uvsq21923164.exo3_3;
+
+
+
+public class Position {
+private int x;
+private int y;
+
+public Position(int x, int y) {
+	this.x=x;
+	this.y=y;
+}
+
+protected void deplaceX(int nbCase) {
+	this.x=this.x+nbCase;
+}
+
+protected void deplaceY(int nbCase) {
+	this.y=this.y+nbCase;
+}
+
+public int getX() {
+	return this.x;
+}
+
+public int getY() {
+	return this.y;
+}
+}
